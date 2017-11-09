525aa9f005c2f86d8c34dcead501507ed55625fb
Michał Drozd <michaldrozd@protonmail.ch>
2017-11-09 16:01:24 +0100

    ...

--- CarDealership/AdminScreen.cs
+++ CarDealership/AdminScreen.cs
@@ -18,5 +18,33 @@ namespace CarDealership
             this.NextScreen = Screens.Exit;
 
         }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            var database = new DealershipDataContext();
+            database.ExecuteCommand("EXEC dbo.reset;");
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void LogoutButton_Click(object sender, EventArgs e)
+        {
+            this.NextScreen = Screens.LoginScreen;
+            this.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void EmployeesTab_Enter(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
